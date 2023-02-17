c4d72986e591133c6ddf82ea98ea11741b1907ca
Anthogr <87010226+Anthogr@users.noreply.github.com>
2023-02-17 15:08:27 +0100

    add ability to edit section name

--- app.py
+++ app.py
@@ -4,6 +4,7 @@
 
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import select
 import markdown
 from turbo_flask import Turbo
 
@@ -38,7 +39,7 @@ class Task(db.Model):
     status = db.Column(db.String, default="toDo")
     priority = db.Column(db.String, default="cool")
     stuck = db.Column(db.String, default="off")
-    type = db.Column(db.String, default="noType")
+    # type_ = db.Column(db.String, default="noType_")
     #def __init__(self, content):
     #    self.content = content
     #    self.status = "todo"
@@ -51,30 +52,59 @@ db.create_all()
 
 # INIT fake db
 #-------------------------------------------------#
-# task1 = Task(content='Update task status toDo > Progress > Done', section='My TODO APPLICATION', status='done')
-# task2 = Task(content='Edit: try with pop up ?', section='My TODO APPLICATION', status='done')
-# task3 = Task(content='Add task: With pop up as well ??', section='My TODO APPLICATION', status='done')
-# task4 = Task(content='Implement ability to add section', section='My TODO APPLICATION', status='done')
-# task5 = Task(content='Reload only turbo frame specific section for **add**/**edit-status**/**edit**/**delete**', section='My TODO APPLICATION', status='done')
-# task6 = Task(content='Reduce sizing(bit too big)', section='My TODO APPLICATION', status='done')
-# task7 = Task(content='**Reset form input ** when hitting cancel button or adding new task', section='My TODO APPLICATION', status='done')
-# task8 = Task(content='Add markdown management', section='My TODO APPLICATION', status='done')
-# task9 = Task(content='`app.config[SECRET_KEY] = ... ` ?', section='My TODO APPLICATION', status='done')
-# task10 = Task(content='Format markdown !!!', section='My TODO APPLICATION', status='done')
-# task11 = Task(content='**BUG ** - Adding new section mess up with the turbo frame...', section='My TODO APPLICATION', status='done')
-# task12 = Task(content='**Improvement ** - Update dynamically `< datalist id = "sectionList" >` for *Add Task * button ([Soluce ?](https: // stackoverflow.com/questions/50141756/datalist-from-python-list) with ```@ app.route("/add-image", methods=["POST", ]) def add_image():     sequence += 1     html = render_template("_image.html", seq=sequence)      return turbo.stream([turbo.append(html, target="images"),         turbo.update(sequence, target="counter"), ])``` from [here](https: // eduardovra.github.io/building-two-sample-apps-using-hotwire-and-flask /))', section='My TODO APPLICATION', status='done')
-# task13 = Task(content='**Improvement ** - Make `inputs` in *Add Task * form required with javascript: `IF[  # section or #task] is empty -> prevent close Add form ELSE close`', section='My TODO APPLICATION', status='done')
-# task14 = Task(content='Add * priority * flag for tasks',section='My TODO APPLICATION', status='done')
-# task15 = Task(content='**ADD ** - Flash message to notify when new section created on the right', section='My TODO APPLICATION', status='done')
-# task16 = Task(content='** ADD ** - block code(Soluce - replace `input` by `textarea`)[markdown formatting](https://daringfireball.net/projects/markdown/syntax  # code)', section='My TODO APPLICATION', status='done')
-# task17 = Task(content='Add login', section='My TODO APPLICATION', status='done')
-# task18 = Task(content='Containerize with **Docker**', section='My TODO APPLICATION', status='done')
-# task19 = Task(content='Deploy(**Heroku ** or other ???)', section='My TODO APPLICATION', status='done')
-# task20 = Task(content='Integrate ** Mosaix**', section='Netcdf Editor App', status='done')
-# task21 = Task(content='Debug ** mosaix ** branch local deployment', section='Netcdf Editor App', status='done')
-# task22 = Task(content='Try a simple config for **PISCES OFFLINE**', section='PISCES', status='done')
-# task23 = Task(content='**ADD** - task classification ???', section='My TODO APPLICATION', status='done')
-# task24 = Task(content='**ADD** - implement sessions for users (???)', section='My TODO APPLICATION', status='done')
+# task1  = Task(content='Update task status toDo > Progress > Done', section='My TODO APPLICATION', status='done')
+# task2  = Task(content='Edit: try with pop up ?', section='My TODO APPLICATION', status='done')
+# task3  = Task(content='Add task: With pop up as well', section='My TODO APPLICATION', status='done')
+# task4  = Task(content='Reload only turbo frame specific section for **add**/**edit-status**/**edit**/**delete**', section='My TODO APPLICATION', status='done')
+# task5  = Task(content='Reduce sizing(bit too big)', section='My TODO APPLICATION', status='done')
+# task6  = Task(content='**Reset form input ** when hitting cancel button or adding new task', section='My TODO APPLICATION', status='done')
+# task7  = Task(content='Add markdown management', section='My TODO APPLICATION', status='done')
+# task8  = Task(content='`app.config[SECRET_KEY] = ... ` ?', section='My TODO APPLICATION', status='done')
+# task9  = Task(content='Format markdown !!!', section='My TODO APPLICATION', status='done')
+# task10 = Task(content='**BUG ** - Adding new section mess up with the turbo frame...', section='My TODO APPLICATION', status='done')
+# task11 = Task(content='**Improvement ** - Update dynamically `< datalist id = "sectionList" >` for *Add Task * button ([Soluce ?](https: // stackoverflow.com/questions/50141756/datalist-from-python-list) with ```@ app.route("/add-image", methods=["POST", ]) def add_image():     sequence += 1     html = render_template("_image.html", seq=sequence)      return turbo.stream([turbo.append(html, target="images"),         turbo.update(sequence, target="counter"), ])``` from [here](https: // eduardovra.github.io/building-two-sample-apps-using-hotwire-and-flask /))', section='My TODO APPLICATION', status='done')
+# task12 = Task(content='**Improvement ** - Make `inputs` in *Add Task* form required with javascript: `IF[  # section or #task] is empty -> prevent close Add form ELSE close`', section='My TODO APPLICATION', status='done')
+# task13 = Task(content='Add * priority * flag for tasks', section='My TODO APPLICATION', status='done')
+# task14 = Task(content='**ADD ** - Flash message to notify when new section created on the right', section='My TODO APPLICATION', status='toDo')
+# task15 = Task(content='** ADD ** - block code for task entry to allow more flexibility (Soluce - replace `input` by `textarea`) [markdown formatting](https://daringfireball.net/projects/markdown/syntax  # code)', section='My TODO APPLICATION', status='toDo')
+# task16 = Task(content='Deploy(**Heroku ** or other ???)', section='My TODO APPLICATION', status='toDo')
+# task17 = Task(content='**v1** - Ability to create session with login + containerize with Docker + get real-time changes between several clients connected to the same session', section='My TODO APPLICATION', status='progress')
+# task18 = Task(content='**v2** - Implement multiple users per session', section='My TODO APPLICATION', status='toDo')
+# task19 = Task(content='**v3** - Messaging service for each task allowing users to discuss + user be attributed to tasks -  **CANCELLED ???**', section='My TODO APPLICATION', status='toDo')
+# task20 = Task(content='**ADD** - Button at the end of each section to add task faster + *add task* button becomes *add section* => implement to ability to add section independently from tasks - **MAYBE NOT !!!** Cumbersome... Just add task at top remains add task with ability to *choose/create* new section and add button add task at bottom of each section to add task in that relative section', section='My TODO APPLICATION', status='progress')
+# task21 = Task(content='**ADD** - Drag and drop list order (source [here](https://tutorial101.blogspot.com/2021/01/python-flask-dynamic-drag-and-drop.html) and [here](https://www.geeksforgeeks.org/how-to-create-a-drag-and-drop-feature-for-reordering-the-images-using-html-css-and-jqueryui/))', section='My TODO APPLICATION', status='toDo')
+# task22 = Task(content='**ADD** - Make nav_bar + new task button stay on top of screen when scrolling down', section='My TODO APPLICATION', status='toDo')
+# task23 = Task(content='**ADD** - Ability to rename section', section='My TODO APPLICATION', status='progress')
+# task24 = Task(content='**ADD** - ability to handle variable number of sub basins (Jean-Baptiste) ', section='Diag_CM5A2', status='toDo')
+# task25 = Task(content='**IMPROVEMENT** - Change structure: task name + task description', section='My TODO APPLICATION', status='progress')
+# task26 = Task(content='**CREATE** - Store source code on **GitHub** (list all implementations of JB and Marie)', section='PISCES OFFLINE', status='progress')
+# task27 = Task(content='**ADD** - Ability to change section or task order', section='My TODO APPLICATION', status='toDo')
+# task28 = Task(content='**CHECK** - See if the JB/MARIE workflow for *adapting concentrations* and *damping* are "mandatory". [**Not very important for now**]', section='PISCES OFFLINE', status='done')
+# task29 = Task(content='**Routing** - Comprendre les I/O de Wesley et comparer aux scripts de JB en f90', section='Netcdf_Editor_App', status='done')
+# task30 = Task(content='**ADD** - Take in account integration of var *siconc* and *wocetr_eff* in with script `UpdateVariable.ksh`', section='PISCES OFFLINE', status='done')
+# task31 = Task(content='**BUG** - At *Regrid* step, if input file has *coordinate* name different from *dim* name => `ERROR` (can be solved but not done because not supposed to be an issue)', section='Netcdf_Editor_App', status='toDo')
+# task32 = Task(content='**MOSAIX** - Fix version with revision no *6245*', section='Netcdf_Editor_App', status='done')
+# task33 = Task(content='**ADD** - Add Archive system (add entry in database `archive = True/False`) main page displays database entry with `archive = False` and New page named *Archive* displays database database entry with `archive = True`. Add archive button at section level close to *rename section name*', section='My TODO APPLICATION', status='toDo')
+# task34 = Task(content='**REDESIGN** (**v3**) - Change display structure. *Sections* are displayed in a column table on left and selecting a section in this table display the *section table* with its tasks on the right. It will make de display cleaner (only 1 *section table* displayed at a time) + For each section on the *Section table* on the left colored patches showing nb of tasks no started, nb in progress and number done ! ![](static/images/Redesign_app.png)', section='My TODO APPLICATION', status='toDo')
+# task35 = Task(content='**Add** - Ability to add figure from local computer ???', section='My TODO APPLICATION', status='toDo')
+# task36 = Task(content='**BUG** - Code is not working anymore neither on frontal nor on calculation node - Try to make working at least `configure_paleo_pisces.py` and `init_paleo_pisces.py` and then maybe check for installing and compiling PISCES on *gen2212* and then replace `Install_paleo_pisces.py` by a simple script copying pisces installed on *gen2212* on user work space -', section='PISCES OFFLINE', status='done')
+# task37 = Task(content='**configure_paleo_pisces.py** on *FRONTALE* - Bug when executing `./UpdateVariable_sic.ksh`: `ncks` and `ncrename` commands not found. Solving -> `ml load nco/4.9.2`', section='PISCES OFFLINE', status='done')
+# task38 = Task(content='**configure_paleo_pisces.py** on *FRONTALE* - New bug at *Compiling weight tool...* step', section='PISCES OFFLINE', status='done')
+# task39 = Task(content='**install_paleo_pisces.py** on *FRONTALE* - Bug when compiling *XIOS* -> `Loading mpi/openmpi/2.0.4 ERROR: cant read "errorCode": no such variable`. Solving -> in `/ccc/work/cont003/gen2212/gramoula/[PALEOPISCES_FOLDER]/modipsl/modeles/XIOS/arch/arch-X64_IRENE.env` replace *module load mpi/openmpi/2.0.4* by *module load mpi/openmpi/4.1.4* **FALSE !!!** - It doesnt solve issue... **SOLVE** - Use Stock IRENE env (no ml load in bashrc) OR use IPSL env !!! -> [here](https://forge.ipsl.jussieu.fr/igcmg_doc/wiki/Doc/ComputingCenters/TGCC/Irene#Generalenvironment)', section='PISCES OFFLINE', status='done')
+# task40 = Task(content='**TIPS** - Modules conseillés avec `python3/3.8.10` : ` intel/20` et `mpi/openmpi/4.1.4`', section='PISCES OFFLINE', status='done')
+# task41 = Task(content='**FIX** - Concernant linstallation/compilation du modèle : voir ce qui peut être effectué en amont au maximum (`install_paleo_pisces.py` et peut être une partie de `configure_paleo_pisces.py`)', section='PISCES OFFLINE', status='done')
+# task42 = Task(content='**install_paleo_pisces.py** - Bug at *Compile NEMO* step. Ive checked the `modipsl/modeles/NEMOGCM/CONFIG/ORCA2_OFF_PISCES/cpp_ORCA2_OFF_PISCES.fcm` file after svn (It might have changed on svn repo) but no modifications made (Just need to add key_xios2 as mentioned in Maries doc)', section='PISCES OFFLINE', status='done')
+# task43 = Task(content='**TIPS ** - Pour installer/compiler * NEMO * sur * IRENE * dans le terminal sans passer par `install_paleo_pisces.py` regarder début du script `pisces_modules/process.py`: les commandes bash y sont indiquées', section='PISCES OFFLINE', status='done')
+# task44 = Task(content='**BUG ** - Build of MOSAIX docker image failed on Github', section='Netcdf_Editor_App', status='done')
+# task45 = Task(content='**NEXT STEP ** - Scripts are running correctly. Next step is to check no error occurs during model run ! (doc has been updated for changes mades related to environment to load)', section='PISCES OFFLINE', status='done')
+# task46 = Task(content='**UPDATE ** - Documentation separate loading env and running script(*RUN_Diag.sh * becomes * load_env.sh*) then use command `python3 Diag_CM5A2.py`', section='Diag_CM5A2', status='done')
+# task47 = Task(content='**UPDATE ** - Code seems to work on frontale with *python 3.8.10 * (maybe test with *python 3.7.5 * as well). If it works, update doc if necessary - **UPDATE ** - Doesnt work with *python 3.8.10*. Used * Python 3.7.5 * instead.', section='Diag_CM5A2', status='progress')
+# task48 = Task(content='**GITHUB ** - Clean local repository by keeping one version', section='PISCES OFFLINE', status='progress')
+# task49 = Task(content='**ADD ** -  Ability to clone a workflow. With this, we can use a paleogeography generate all files and if we want to modify Bathy for example, we can duplicate workflow and modify it and original workflow is not modified(Request Yannick)', section='Netcdf_Editor_App', status='toDo')
+# task50 = Task(content='**BUG ** - At * Passage Problem * step when saving sometime, *ahmcoef * data is saved in *heatflow * file and sometime it is reversed. **SOLVING ** - Add a `db.execute("BEGIN IMMEDIATE")` in `db.py` in function `save_file_to_db` *????*', section='Netcdf_Editor_App', status='done')
+
+
+
 
 # db.session.add(task1)
 # db.session.add(task2)
@@ -100,12 +130,38 @@ db.create_all()
 # db.session.add(task22)
 # db.session.add(task23)
 # db.session.add(task24)
+# db.session.add(task25)
+# db.session.add(task26)
+# db.session.add(task27)
+# db.session.add(task28)
+# db.session.add(task29)
+# db.session.add(task30)
+# db.session.add(task31)
+# db.session.add(task32)
+# db.session.add(task33)
+# db.session.add(task34)
+# db.session.add(task35)
+# db.session.add(task36)
+# db.session.add(task37)
+# db.session.add(task38)
+# db.session.add(task39)
+# db.session.add(task40)
+# db.session.add(task41)
+# db.session.add(task42)
+# db.session.add(task43)
+# db.session.add(task44)
+# db.session.add(task45)
+# db.session.add(task46)
+# db.session.add(task47)
+# db.session.add(task48)
+# db.session.add(task49)
+# db.session.add(task50)
 # db.session.commit()
 #-------------------------------------------------#
 
 status=["toDo","progress","done"]
 priority=["cool","warm","hot"]
-type=["noType", "addImprove", "bug", "genious"]
+# type_=["noType", "addImprove", "bug", "genious"]
 # addImprove
 # <i class="fa-solid fa-plus"></i>
 # <i class="fa-solid/regular fa-square-plus"></i>
@@ -214,12 +270,12 @@ def delete_task(task_id):
         turbo.update(htmlContent, target="flex-row")
     ])
 
-#-------------------------EDIT SECTION - TO DO------------------------#
-@app.route('/edit_section/<int:task_id>', methods=['POST'])
-def edit_section(task_id):
-    content = request.form.get('content')
-    task = Task.query.get(task_id)
-    task.content = content
+
+@app.route('/edit_section/<section_name>', methods=['POST'])
+def edit_section(section_name):
+    new_section_name = request.form.get('content')
+    # tasks_filetered = Task.query.filter_by(section=section_name).all()
+    Task.query.filter_by(section=section_name).update(dict(section=new_section_name))
     db.session.commit()
 
     # Update only specific parts of main html page
@@ -228,7 +284,6 @@ def edit_section(task_id):
         turbo.update(htmlSectionList, target="sectionList"),
         turbo.update(htmlContent, target="flex-row")
     ])
-#--------------------------------------------------------------------#
 
 @app.route('/edit/<int:task_id>', methods=['POST'])
 def edit_task(task_id):
